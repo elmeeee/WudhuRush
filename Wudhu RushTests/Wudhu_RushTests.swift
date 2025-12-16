59ef64099cc3de63786a3f1221d8f829f4ad1859
elmeeee <37185272+elmeeee@users.noreply.github.com>
2025-12-16 16:30:24 +0700

    Initial Commit

---
+++ Wudhu RushTests/Wudhu_RushTests.swift
@@ -0,0 +1,17 @@
+//
+//  Wudhu_RushTests.swift
+//  Wudhu RushTests
+//
+//  Created by Elmee on 16/12/2025.
+//
+
+import Testing
+@testable import Wudhu_Rush
+
+struct Wudhu_RushTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
